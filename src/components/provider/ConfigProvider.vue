3447f7057fc7b064184c2c634d0bdd3d4a6f139e
nononavasgit <janavasgarcia1@gmail.com>
2026-09-26 14:44:08 +0200

    Add configurable neutral color themes

--- src/components/provider/ConfigProvider.vue
+++ src/components/provider/ConfigProvider.vue
@@ -3,6 +3,11 @@ import { ConfigProvider as RekaConfigProvider, useDirection } from 'reka-ui'
 import { computed, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { getTextDirection } from '@/i18n'
+import type { NeutralColor } from '../../types/theme'
+
+const props = withDefaults(defineProps<{ neutral?: NeutralColor }>(), {
+  neutral: 'neutral',
+})
 
 const { locale } = useI18n({ useScope: 'global' })
 const localeDirection = computed(() => getTextDirection(locale.value))
@@ -14,6 +19,16 @@ watch(
     if (typeof document !== 'undefined') {
       document.documentElement.lang = value
       document.documentElement.dir = getTextDirection(value)
+    }
+  },
+  { immediate: true },
+)
+
+watch(
+  () => props.neutral,
+  (value) => {
+    if (typeof document !== 'undefined') {
+      document.documentElement.dataset.neutral = value
     }
   },
   { immediate: true },
--- src/components/provider/index.ts
+++ src/components/provider/index.ts
@@ -1,1 +1,2 @@
 export { default as ConfigProvider } from './ConfigProvider.vue'
+export { neutralColors, type NeutralColor } from '../../types/theme'
